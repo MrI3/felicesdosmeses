293d494c3d9501d5ae2ebaf8cbfdc1dae5912ac9
MrI3 <gt.isaac3@gmail.com>
2024-02-12 21:39:11 -0600

    Add orientation function

--- src/components/LocationDistance.js
+++ src/components/LocationDistance.js
@@ -45,29 +45,28 @@ const LocationDistance = ({ targetLatitude, targetLongitude }) => {
 
   const calculateDistance = (userLatitude, userLongitude) => {
     const earthRadius = 6371e3; // meters
-
     const calculateHaversineDistance = (lat1, lon1, lat2, lon2) => {
       const toRadians = degree => degree * Math.PI / 180;
       const deltaLat = toRadians(lat2 - lat1);
       const deltaLon = toRadians(lon2 - lon1);
-
+      
       const a = Math.sin(deltaLat / 2) * Math.sin(deltaLat / 2) +
                 Math.cos(toRadians(lat1)) * Math.cos(toRadians(lat2)) *
                 Math.sin(deltaLon / 2) * Math.sin(deltaLon / 2);
       const c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1 - a));
-
+      
       return earthRadius * c;
     };
-
+  
     const distances = {
       north: (targetLatitude > userLatitude) ? calculateHaversineDistance(userLatitude, userLongitude, targetLatitude, userLongitude) : 0,
       south: (targetLatitude < userLatitude) ? calculateHaversineDistance(userLatitude, userLongitude, targetLatitude, userLongitude) : 0,
       east: (targetLongitude > userLongitude) ? calculateHaversineDistance(userLatitude, userLongitude, userLatitude, targetLongitude) : 0,
       west: (targetLongitude < userLongitude) ? calculateHaversineDistance(userLatitude, userLongitude, userLatitude, targetLongitude) : 0,
     };
-
+  
     setDistance(distances);
-  };
+  };  
 
   return (
     <div>
